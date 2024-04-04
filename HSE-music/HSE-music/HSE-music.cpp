55a9891f76a517b58d35e2f86d835699d991d903
stasy <94540033+shepnst@users.noreply.github.com>
2024-04-05 01:38:35 +0300

    Update HSE-music.cpp

--- HSE-music/HSE-music/HSE-music.cpp
+++ HSE-music/HSE-music/HSE-music.cpp
@@ -1,5 +1,24 @@
-﻿#include <iostream>
-
+#include <iostream>
+#include "person.h"
+#include "artist.h"
+#include "song.h"
+//#include "band.h"
+#include "genre.h"
 int main() {
-
+	string name("Egor");
+	int age = 30;
+	string sex("male");
+	string name2("Egor Kreed");
+	Artist Egor(name, age, sex, name2, 15000);
+	cout << Egor.getPseudonym() << endl;
+	Person Vlad(name, age, sex);
+	cout << Vlad<<endl;
+	string song("Alarm");
+	Song Alarm(name, age, sex, name2, 15000, song, 5, 2015);
+	cout << Alarm << endl;
+	string alb_title("super");
+	string genre_type("Rock");
+	string instrument("Guitar");
+	Genre Rock(name, age, sex, name2, 15000, song, 5, 2015, genre_type, instrument);
+	cout << Rock << endl;
 }
