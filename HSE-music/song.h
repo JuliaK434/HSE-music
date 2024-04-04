4a9aff35450eaa7e0b06fbd41415d5e5831e5537
JuliaK434 <146024526+JuliaK434@users.noreply.github.com>
2024-04-05 00:09:54 +0300

    Add files via upload

---
+++ HSE-music/song.h
@@ -0,0 +1,50 @@
+#ifndef SONG_H
+#define SONG_H
+
+#include <iostream>
+#include "artist.h"
+#include "person.h"
+
+class Song : public Artist {
+private:
+	string title;
+	int duration;
+	int yearWriting;
+	Person* name;
+public:
+	Song() {
+		duration = 0;
+		yearWriting = 0;
+	}
+	Song(string& name, int& age, string& gender, string& pseudonym, int numberAuditions, string& title, int duration, int yearWriting)
+		:Artist(name, age, gender, pseudonym, numberAuditions) {
+		this->title = title;
+		this->duration = duration;
+		this->yearWriting = yearWriting;
+	}
+	string getTitle() {
+		return title;
+	}
+	void setTitle(string& title) {
+		this->title = title;
+	}
+	int getDuration() {
+		return duration;
+	}
+	void setDuration(int duration) {
+		this->duration = duration;
+	}
+	int getYearWriting() {
+		return yearWriting;
+	}
+	void setYearWriting(int yearWriting) {
+		this->yearWriting = yearWriting;
+	}
+	friend ostream& operator<<(ostream& os, const Song& s);
+};
+ostream& operator<<(ostream& os, const Song& s) {
+	os << "Title: " << s.title << ", Performed by: " << s.name;
+	return os;
+}
+	
+#endif //SONG_H
