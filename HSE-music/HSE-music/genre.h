532d577b97ce65ec97f5cc19f14b41d72794548c
stasy <94540033+shepnst@users.noreply.github.com>
2024-04-05 01:38:10 +0300

    Update genre.h

--- HSE-music/HSE-music/genre.h
+++ HSE-music/HSE-music/genre.h
@@ -14,7 +14,7 @@ public:
 		genre_type = "None";
 		instrument = "None";
 	}
-	Genre(string& name, int& age, string& gender, string& pseudonym, int numberAuditions, string& title, int duration, int yearWriting, string& album_title, int num_songs)
+	Genre(string& name, int& age, string& gender, string& pseudonym, int numberAuditions, string& title, int duration, int yearWriting, string&genre_type, string& instrument)
 		:Song(name, age, gender, pseudonym, numberAuditions, title, duration, yearWriting) {
 		this->genre_type = genre_type;
 		this->instrument = instrument;
@@ -35,12 +35,12 @@ public:
 	void setInstrument(int instrument) {
 		this->instrument = instrument;
 	}
- friend ostream& operator<<(ostream& os, const Genre& g);
+	friend ostream& operator<<(ostream& os, const Genre& g);
 };
 
 ostream& operator<<(ostream& os, const Genre& g) {
-    os << "Genre: " << g.name;
-    return os;
+	os << "Genre: " << g.genre_type;
+	return os;
 }
 
 #endif //GENRE_H
