bc1bc58547911e7867cc340560814237d93302ca
JuliaK434 <146024526+JuliaK434@users.noreply.github.com>
2024-04-05 18:35:50 +0300

    Update genre.h

--- HSE-music/HSE-music/genre.h
+++ HSE-music/HSE-music/genre.h
@@ -10,34 +10,14 @@ private:
 	string instrument;
 
 public:
-	Genre() {
-		genre_type = "None";
-		instrument = "None";
-	}
-	Genre(string& name, int& age, string& gender, string& pseudonym, int numberAuditions, string& title, int duration, int yearWriting, string&genre_type, string& instrument)
-		:Song(name, age, gender, pseudonym, numberAuditions, title, duration, yearWriting) {
-		this->genre_type = genre_type;
-		this->instrument = instrument;
-	}
-
-	string getGenre_type() {
-		return genre_type;
-	}
-
-	void setGenre_type(string& genre_type) {
-		this->genre_type = genre_type;
-	}
-
-	string getInstrument() {
-		return instrument;
-	}
-
-	void setInstrument(int instrument) {
-		this->instrument = instrument;
-	}
-	friend ostream& operator<<(ostream& os, const Genre& g);
+	Genre();
+	Genre(string& name, int& age, string& gender, string& pseudonym, int numberAuditions, string& title, int duration, int yearWriting, string& genre_type, string& instrument);
+	string getGenre_type();
+	void setGenre_type(string& genre_type);
+	string getInstrument();
+	void setInstrument(int instrument);
+	inline friend ostream& operator<<(ostream& os, const Genre& g);
 };
-
 ostream& operator<<(ostream& os, const Genre& g) {
 	os << "Genre: " << g.genre_type;
 	return os;
