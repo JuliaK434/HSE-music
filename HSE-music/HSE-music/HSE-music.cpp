b1c7dca9d90a08fb0dfe76c8668a92190ecb9c83
JuliaK434 <146024526+JuliaK434@users.noreply.github.com>
2024-04-05 18:33:34 +0300

    Update HSE-music.cpp

--- HSE-music/HSE-music/HSE-music.cpp
+++ HSE-music/HSE-music/HSE-music.cpp
@@ -9,25 +9,33 @@
 int main() {
 	string name("Egor Bulatkin");
 	string title("TOP 10 EGOR KREED");
-	int age = 30;
+	int age = 29;
 	string gender("male");
 	string pseudonim("Egor Kreed");
 	Artist Egor(name, age, gender, pseudonim, 4500000);
 	cout << Egor.getPseudonym() << endl;
 	Person EgorK(name, age, gender);
 	cout << EgorK << endl;
+	Band EK(name, age,gender,pseudonim, 4500000, 2011, 0);
+	cout << "Band: " << EK << endl;
 	string song1("LAMBO URUS");
 	string song2("PUSSY BOY");
+	string song3("ALARM");
+	string song4("WE GOT GET LOVE");
+	string song5("TARO");
 	Song LAMBOURUS(name, age, gender, pseudonim, 4500000, song1, 2.28, 2021);
 	Song PUSSYBOY(name, age, gender, pseudonim, 4500000, song2, 2.12, 2021);
+	Song ALARM(name, age, gender, pseudonim, 4500000, song3, 3.24, 2015);
+	Song WEGOTGETLOVE(name, age, gender, pseudonim, 4500000, song4, 2.14, 2022);
+	Song TARO(name, age, gender, pseudonim, 4500000, song5, 3.20, 2023);
 	cout << LAMBOURUS << endl;
 	cout << PUSSYBOY << endl;
-	string alb_title("TOP 10 EGOR KREED");
+	string alb_title("TOP 5: EGOR KREED");
 	string genre_type("Disko pop");
 	string instrument("Computer music");
 	Genre DISCOPOP(name, age, gender, pseudonim, 4500000, song1, 5, 2015, genre_type, instrument);
 	cout << DISCOPOP << endl;
 	Album Hit(title);
-	Hit + LAMBOURUS+PUSSYBOY;
+	Hit + LAMBOURUS + PUSSYBOY + ALARM + WEGOTGETLOVE + TARO;
 	cout << Hit;
 }
