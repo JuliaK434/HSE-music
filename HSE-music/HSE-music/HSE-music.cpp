143ca6dd6d4d5118b8ca9794f42681ed0ccb217d
JuliaK434 <146024526+JuliaK434@users.noreply.github.com>
2024-04-05 11:58:15 +0300

    Update HSE-music.cpp

--- HSE-music/HSE-music/HSE-music.cpp
+++ HSE-music/HSE-music/HSE-music.cpp
@@ -2,23 +2,32 @@
 #include "person.h"
 #include "artist.h"
 #include "song.h"
-//#include "band.h"
+#include "band.h"
 #include "genre.h"
+#include "album.h"
+
 int main() {
-	string name("Egor");
+	string name("Egor Bulatkin");
+	string title("TOP 10 EGOR KREED");
 	int age = 30;
-	string sex("male");
-	string name2("Egor Kreed");
-	Artist Egor(name, age, sex, name2, 15000);
+	string gender("male");
+	string pseudonim("Egor Kreed");
+	Artist Egor(name, age, gender, pseudonim, 4500000);
 	cout << Egor.getPseudonym() << endl;
-	Person Vlad(name, age, sex);
-	cout << Vlad<<endl;
-	string song("Alarm");
-	Song Alarm(name, age, sex, name2, 15000, song, 5, 2015);
-	cout << Alarm << endl;
-	string alb_title("super");
-	string genre_type("Rock");
-	string instrument("Guitar");
-	Genre Rock(name, age, sex, name2, 15000, song, 5, 2015, genre_type, instrument);
-	cout << Rock << endl;
+	Person EgorK(name, age, gender);
+	cout << EgorK << endl;
+	string song1("LAMBO URUS");
+	string song2("PUSSY BOY");
+	Song LAMBOURUS(name, age, gender, pseudonim, 4500000, song1, 2.28, 2021);
+	Song PUSSYBOY(name, age, gender, pseudonim, 4500000, song2, 2.12, 2021);
+	cout << LAMBOURUS << endl;
+	cout << PUSSYBOY << endl;
+	string alb_title("TOP 10 EGOR KREED");
+	string genre_type("Disko pop");
+	string instrument("Computer music");
+	Genre DISCOPOP(name, age, gender, pseudonim, 4500000, song1, 5, 2015, genre_type, instrument);
+	cout << DISCOPOP << endl;
+	Album Hit(title);
+	Hit + LAMBOURUS+PUSSYBOY;
+	cout << Hit;
 }
