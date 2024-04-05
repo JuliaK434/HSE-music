53bf8f6132fe37a213fed1e5c1f030e46b2a172b
JuliaK434 <146024526+JuliaK434@users.noreply.github.com>
2024-04-05 18:40:28 +0300

    Add files via upload

---
+++ HSE-music/HSE-music/band.cpp
@@ -0,0 +1,23 @@
+#include "band.h"
+
+Band::Band() {
+    yearCreate = 0;
+    yearBreakup = 0;
+}
+Band::Band(string& name, int& age, string& gender, string& pseudonym, int numberAuditions, int yearCreate, int yearBreakup)
+    :Artist(name, age, gender, pseudonym, numberAuditions) {
+    this->yearCreate = yearCreate;
+    this->yearBreakup = yearBreakup;
+}
+int Band::getYearCreate() {
+    return yearCreate;
+}
+void Band::setYearCreate(int yearCreate) {
+    this->yearCreate = yearCreate;
+}
+int Band::getBreakup() {
+    return yearBreakup;
+}
+void Band::setYearBreakup(int yearBreakup) {
+    this->yearBreakup = yearBreakup;
+}
